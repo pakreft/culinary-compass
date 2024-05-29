a417ac84ed7f836db6e8357d4d13a2aa10c6e21f
pakreft <113921665+pakreft@users.noreply.github.com>
2024-05-30 01:25:28 +0200

    Change background colors

--- src/navigation/TabBar/styles.js
+++ src/navigation/TabBar/styles.js
@@ -1,13 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  sceneContainer: {
-    backgroundColor: 'yellow',
-  },
-  header: {
-    backgroundColor: 'blue',
-  },
-  tabBar: {
-    backgroundColor: 'pink',
-  },
+  sceneContainer: {},
+  header: {},
+  tabBar: {},
 });
