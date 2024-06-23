a7ef98fc870e77f17f3deb6e3605fbf0b4512057
lennyb99 <lennogle@gmail.com>
2024-06-23 15:20:55 +0200

    code cleanup

--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -2,57 +2,6 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Button, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-/*
-const ShoppingListItem = ({ item, onDelete, onToggleDone, showDeleteButton }) => (
-  <TouchableOpacity style={styles.itemContainer}>
-    <View>
-      <Text style={styles.itemText}>{item.name}</Text>
-      <Text style={styles.itemAmount}>{item.amount}</Text>
-    </View>
-    <View style={styles.buttonsContainer}>
-      <View style={styles.buttonWrapper}>
-        <Button
-          title="Done"
-          color={item.done ? 'gray' : 'green'}
-          onPress={() => onToggleDone(item.id)}
-        />
-      </View>
-      {showDeleteButton && (
-        <View style={styles.buttonWrapper}>
-          <Button title="Delete" color="red" onPress={() => onDelete(item.id)} />
-        </View>
-      )}
-    </View>
-  </TouchableOpacity>
-);
-
-const styles = StyleSheet.create({
-  itemContainer: {
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  itemText: {
-    fontSize: 20,
-  },
-  itemAmount: {
-    fontSize: 14,
-    color: '#888',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-  },
-  buttonWrapper: {
-    marginLeft: 10,
-  },
-});
-
-export default ShoppingListItem;
-*/
-
 const ShoppingListItem = ({ item, onDelete, onToggleDone, showDeleteButton, mode }) => {
   if (mode === "shopping") {
     return (
