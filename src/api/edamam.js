5f1f0743079ce9a3a1d64311402b1c6783258a47
pakreft <113921665+pakreft@users.noreply.github.com>
2024-06-26 22:44:01 +0200

    Add func for recipe search v2

--- src/api/edamam.js
+++ src/api/edamam.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const APP_ID = process.env.EXPO_PUBLIC_API_ID;
 const APP_KEY = process.env.EXPO_PUBLIC_API_KEY;
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
+const BASE_URL_V2 = process.env.EXPO_PUBLIC_BASE_URL_V2;
 
-export const getRecipes = async (query) => {
+export const getRecipes = async () => {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
@@ -19,3 +20,31 @@ export const getRecipes = async (query) => {
     throw error;
   }
 };
+
+/**
+ *
+ * @param {Object} params See {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
+ * for how it's structured. Params 'type', 'app_id', 'app_key' can't be changed.
+ * @returns {Object} See {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
+ * for how it's structured.
+ */
+export async function fetchRecipes(params) {
+  const constantParams = {
+    type: 'public',
+    app_id: APP_ID,
+    app_key: APP_KEY,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL_V2, {
+      params: {
+        ...params,
+        ...constantParams,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+    throw error;
+  }
+}
