7b0c082ad3916a8e593eda8a5e015b9d0b49f404
pakreft <113921665+pakreft@users.noreply.github.com>
2024-06-28 23:42:59 +0200

    Add custom serializer to encode arrays correctly

--- src/api/edamam.js
+++ src/api/edamam.js
@@ -2,30 +2,36 @@ import axios from 'axios';
 
 const APP_ID = process.env.EXPO_PUBLIC_APP_ID;
 const APP_KEY = process.env.EXPO_PUBLIC_APP_KEY;
-const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
-const BASE_URL_V2 = process.env.EXPO_PUBLIC_BASE_URL_V2;
+const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL_V2;
 
-export const getRecipes = async () => {
-  try {
-    const response = await axios.get(BASE_URL, {
-      params: {
-        q: query,
-        app_id: APP_ID,
-        app_key: APP_KEY,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching recipes:', error);
-    throw error;
+/**
+ * Encodes the params given to 'fetchRecipes' correctly.
+ */
+function paramsSerializer(params) {
+  const parts = [];
+
+  for (const key in params) {
+    if (Array.isArray(params[key])) {
+      // If key is an array, repeat for every element
+      params[key].forEach((value) => {
+        const encoded = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+        parts.push(encoded);
+      });
+    } else {
+      // Otherwise, encode the key-value pair normally
+      const encoded = `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
+      parts.push(encoded);
+    }
   }
-};
+
+  return parts.join('&');
+}
 
 /**
  *
- * @param {Object} params See 'Recipe Search' {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
+ * @param {Object} params See 'Recipe Search' in {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
  * for how it's structured. Params 'type', 'app_id', 'app_key' can't be changed.
- * @returns {Object} See 'Respone' {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
+ * @returns {Object} See 'Respone' in {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
  * for how it's structured.
  */
 export async function fetchRecipes(params) {
@@ -35,16 +41,23 @@ export async function fetchRecipes(params) {
     app_key: APP_KEY,
   };
 
+  const finalParams = { ...params, ...constantParams };
+
+  //logFullURL(finalParams);
+
   try {
-    const response = await axios.get(BASE_URL_V2, {
-      params: {
-        ...params,
-        ...constantParams,
-      },
+    const response = await axios.get(BASE_URL, {
+      params: finalParams,
+      paramsSerializer: paramsSerializer,
     });
     return response.data;
   } catch (error) {
-    console.error('Error fetching recipes:', error);
     throw error;
   }
+}
+
+function logFullURL(finalParams) {
+  const queryString = paramsSerializer(finalParams);
+  const fullUrl = `${BASE_URL_V2}?${queryString}`;
+  console.log('----- Full URL from axios: ' + fullUrl + ' -----');
 }
