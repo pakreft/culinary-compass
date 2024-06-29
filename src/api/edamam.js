3eed445a3b8f9b0af25328f555bbecdd9691aad3
pakreft <113921665+pakreft@users.noreply.github.com>
2024-06-29 15:42:28 +0200

    Improve documentation

--- src/api/edamam.js
+++ src/api/edamam.js
@@ -5,7 +5,43 @@ const APP_KEY = process.env.EXPO_PUBLIC_APP_KEY;
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL_V2;
 
 /**
- * Encodes the params given to 'fetchRecipes' correctly.
+ *
+ * @param {Object} params An object containing key-value pairs as seen
+ * {@link https://developer.edamam.com/edamam-docs-recipe-api here}.
+ * Params 'type', 'app_id', 'app_key', 'field' can't be changed.
+ *
+ * @param {boolean} logURL Wether to log the full URL encoded by axios. Defaults to false.
+ *
+ * @returns {Object} The response object specified
+ * {@link https://developer.edamam.com/edamam-docs-recipe-api here} or an error.
+ * Access recipes via 'response.hits' and a single one via 'response.hits[x].recipe'.
+ * A recipe has the following keys:
+ */
+export async function fetchRecipes(params, logURL = false) {
+  const constantParams = {
+    type: 'publi',
+    app_id: APP_ID,
+    app_key: APP_KEY,
+    field: [],
+  };
+
+  const finalParams = { ...params, ...constantParams };
+
+  if (logURL) logFullURL(finalParams);
+
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: finalParams,
+      paramsSerializer: paramsSerializer,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Custom serializer to encode arrays correctly.
  */
 function paramsSerializer(params) {
   const parts = [];
@@ -27,37 +63,60 @@ function paramsSerializer(params) {
   return parts.join('&');
 }
 
-/**
- *
- * @param {Object} params See 'Recipe Search' in {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
- * for how it's structured. Params 'type', 'app_id', 'app_key' can't be changed.
- * @returns {Object} See 'Respone' in {@link https://developer.edamam.com/edamam-docs-recipe-api documentation }
- * for how it's structured.
- */
-export async function fetchRecipes(params) {
-  const constantParams = {
-    type: 'public',
-    app_id: APP_ID,
-    app_key: APP_KEY,
-  };
-
-  const finalParams = { ...params, ...constantParams };
-
-  //logFullURL(finalParams);
-
-  try {
-    const response = await axios.get(BASE_URL, {
-      params: finalParams,
-      paramsSerializer: paramsSerializer,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
 function logFullURL(finalParams) {
   const queryString = paramsSerializer(finalParams);
-  const fullUrl = `${BASE_URL_V2}?${queryString}`;
+  const fullUrl = `${BASE_URL}?${queryString}`;
   console.log('----- Full URL from axios: ' + fullUrl + ' -----');
+}
+
+export async function fetchRecipesWithAI(query) {
+  try {
+    const data = {
+      options: {
+        calls: ['search'],
+      },
+      exchange: [
+        {
+          query: 'vegan paleo recipes, 2-3oz protein per serving',
+        },
+      ],
+    };
+
+    axios
+      .post('https://api.edamam.com/api/assistant/v1/query', {
+        options: {
+          calls: ['search'],
+        },
+        exchange: [
+          {
+            query: 'vegan paleo recipes, 2-3oz protein per serving',
+          },
+        ],
+
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        auth: {
+          username: '6ade19a7',
+          password: 'c21c4c9d86daa63669e6ad212ce598d8',
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error('Error response data:', error.response.data);
+          console.error('Error response status:', error.response.status);
+          console.error('Error response headers:', error.response.headers);
+        } else if (error.request) {
+          console.error('Error request:', error.request);
+        } else {
+          console.error('Error message:', error.message);
+        }
+      });
+  } catch (error) {
+    console.error('Error!:', error);
+  }
 }
