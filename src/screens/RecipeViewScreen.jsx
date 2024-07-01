5f5f6f0dfdf708670f6a6efa8e5116f164ab1f07
pakreft <113921665+pakreft@users.noreply.github.com>
2024-07-01 21:52:07 +0200

    Update RecipeViewScreen.jsx

--- src/screens/RecipeViewScreen.jsx
+++ src/screens/RecipeViewScreen.jsx
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native';
+import { useState } from 'react';
 
-export default function RecipeViewScreen({ route, navigation }) {
+import RecipeCard from '../components/RecipeCard';
+
+export default function RecipeViewScreen({ route }) {
+  const [loading, setLoading] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const hits = route.params.res.hits;
+
   return (
     <View style={styles.screen}>
-      <Text>{route.params?.recipes.length}</Text>
+      <FlatList
+        style={styles.list}
+        data={hits}
+        numColumns={2}
+        renderItem={({ item: hit }) => <RecipeCard recipe={hit.recipe} />}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={
+          loading ? (
+            <View
+              style={{
+                height: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <ActivityIndicator />
+            </View>
+          ) : (
+            <View style={{ height: 10 }} />
+          )
+        }
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({ screen: {} });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    paddingHorizontal: 8,
+  },
+  list: {},
+});
